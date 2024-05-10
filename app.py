beaab09c0e8dba0537ee11712e43f5a8ac7d6ee6
Randall-BL <74124774+Randall-BL@users.noreply.github.com>
2024-05-10 02:37:41 -0600

    Tabla sedes

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 from flask import Flask
 from materials import materials
 from config import Config
+from sedes import sedes
 from flask_cors import CORS
-
 app = Flask(__name__)
 CORS(app)
 
 app.config.from_object(Config)
 app.register_blueprint(materials)
+app.register_blueprint(sedes)
 
 if __name__ == '__main__':
     app.run(debug=app.config['DEBUG'], use_reloader=False)
