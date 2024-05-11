f16c5bf5c20392bb8133b4ec831bcd5184ad2ec7
Kearranza <cbkevin.ant@gmail.com>
2024-05-10 18:58:33 -0600

    Agregar import de centros

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from materials import materials
 from config import Config
 from sedes import sedes
+from centros import centros
 from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
