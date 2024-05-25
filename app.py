189098fadb5571513fad0409ee12517eef49a78c
Randall-BL <74124774+Randall-BL@users.noreply.github.com>
2024-05-24 22:21:38 -0600

    Cambios teccolones

--- app.py
+++ app.py
@@ -1,15 +1,20 @@
 from flask import Flask
 from materials import materials
-from config import Config
 from sedes import sedes
 from centros import centros
-from flask_cors import CORS
+from cambios import cambios  
+from config import Config
+
 app = Flask(__name__)
 CORS(app)
 
 app.config.from_object(Config)
+
+# Registro de Blueprints
 app.register_blueprint(materials)
 app.register_blueprint(sedes)
 app.register_blueprint(centros)
+app.register_blueprint(cambios)  
+
 if __name__ == '__main__':
     app.run(debug=app.config['DEBUG'], use_reloader=False)
