e6c07c7dbef908b1421941fd090787b6ebc2215c
Randall-BL <74124774+Randall-BL@users.noreply.github.com>
2024-06-07 05:37:53 -0600

    Usuarios integrados

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from EndPoints.sedes import sedes
 from EndPoints.centros import centros
 from EndPoints.cambios import cambios  
 from Variados.config import Config
+from usuarios import usuarios
 
 app = Flask(__name__)
 CORS(app)
@@ -16,6 +17,7 @@ app.register_blueprint(materials)
 app.register_blueprint(sedes)
 app.register_blueprint(centros)
 app.register_blueprint(cambios)  
+app.register_blueprint(usuarios)
 
 if __name__ == '__main__':
     app.run(debug=app.config['DEBUG'], use_reloader=False)
