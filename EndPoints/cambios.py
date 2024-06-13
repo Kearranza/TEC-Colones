c4d11d73e2fb546a677775dc6b33f0994346dfa4
Randall-BL <74124774+Randall-BL@users.noreply.github.com>
2024-06-13 04:39:55 -0600

    Update cambios.py

--- EndPoints/cambios.py
+++ EndPoints/cambios.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, request, jsonify
-from EndPoints.db import get_all_cambios, get_cambio_by_id, insert_cambio
-from Variados.utils import generate_id
+from db import get_all_cambios, get_cambio_by_id, insert_cambio, update_cambio
+from utils import generate_id
+from datetime import datetime
 
 cambios = Blueprint('cambios', __name__)
 
@@ -21,22 +22,31 @@ def get_cambio(id):
     return jsonify({"error": "No hay datos en la tabla cambios"}), 404
 
 @cambios.route('/cambios', methods=['POST'])
-def create_cambio():
-    """Endpoint to create a new cambio."""
+def create_or_update_cambio():
+    """Endpoint to create or update a cambio."""
     data = request.get_json()
-    required_fields = ['codigo_centro_acopio', 'estudiante', 'monto', 'id_material', 'cantidad']
+    required_fields = ['codigo_centro_acopio', 'estudiante', 'monto', 'id_material', 'cantidad', 'estado']
     if not data or not all(field in data for field in required_fields):
-        return jsonify({'error': 'Missing data, required fields: codigo_centro_acopio, estudiante, monto, id_material, cantidad'}), 400
-    
-    # Validar que la cantidad es un entero y no es negativa
-    if not isinstance(data['cantidad'], int) or data['cantidad'] < 0:
-        return jsonify({'error': 'Cantidad debe ser un número entero no negativo'}), 400
+        return jsonify({'error': 'Missing data, required fields: codigo_centro_acopio, estudiante, monto, id_material, cantidad, estado'}), 400
 
-    data['id'] = generate_id('R-')
-    success, message = insert_cambio(data)
-    if success:
-        # Devuelve el ID del cambio si se creó correctamente
-        return jsonify({'id': data['id'], 'message': message}), 201
+    if data['estado'] not in [0, 1, 3]:
+        return jsonify({'error': 'Invalid state value'}), 400
+
+    cambio_id = data.get('id')
+    if cambio_id:
+        existing_cambio = get_cambio_by_id(cambio_id)
+        if existing_cambio:
+            # Actualizar el cambio existente
+            success, message = update_cambio(cambio_id, data)
+        else:
+            return jsonify({"error": "Cambio ID not found for update"}), 404
     else:
-        # Devuelve un mensaje de error si el cambio no se pudo generar
+        # Crear un nuevo cambio
+        data['id'] = generate_id('R-')
+        data['fecha_transaccion'] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        success, message = insert_cambio(data)
+
+    if success:
+        return jsonify({'id': data['id'], 'message': message}), 201 if not cambio_id else 200
+    else:
         return jsonify({"error": message}), 500
